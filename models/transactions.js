d117a10243a75f30103888d9d9f7da4ebadb17f1
“danielsolaquegit config --global user.email “danielsolaque@pursuit.org <example@email.com>
2023-01-27 21:01:02 -0500

    back end part

---
+++ models/transactions.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    item_name: "Income",
+    amount: 2500,
+    date: "October 30",
+    from: "employer",
+    category: "income",
+    type: "income",
+  },
+  {
+    item_name: "Savings",
+    amount: 7000,
+    date: "August 15",
+    from: "bank",
+    category: "savings",
+    type: "income",
+  },
+  {
+    item_name: "Cat food",
+    amount: 45,
+    date: "September 24",
+    from: "pet store",
+    category: "pets",
+    type: "expense",
+  },
+  {
+    item_name: "Transport",
+    amount: 150,
+    date: "December 16",
+    from: "MTA",
+    category: "transport",
+    type: "expense",
+  },
+];
+
+
